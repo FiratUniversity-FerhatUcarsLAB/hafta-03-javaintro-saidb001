03a08dfd12063212746ad9e02782fe6b811320b4
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-10-23 09:28:58 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,18 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+    String ad = "Muhammed Said";
+    String soyad = "Bestil";
+    String ogrNo = "250542007";
+    String bolum = "Bilgisayar Mühendisliği";
+
+    System.out.println("******************************");
+    System.out.println("*        İSİM KARTI          *");
+    System.out.println("******************************");
+    System.out.println("* Ad       : " + ad);
+    System.out.println("* Soyad    : " + soyad);
+    System.out.println("* Öğrenci No: " + ogrNo);
+    System.out.println("* Bölüm    : " + bolum);
+    System.out.println("******************************");
+}
+
+}
