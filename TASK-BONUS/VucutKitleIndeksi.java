cf780582db0d21d06485847405a0835a9b8f782e
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-10-23 09:38:10 +0300

    Update VucutKitleIndeksi.java

---
+++ TASK-BONUS/VucutKitleIndeksi.java
@@ -0,0 +1,12 @@
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+    double kilo = 70.0;
+    double boy = 1.75;
+    double bmi = kilo / (boy * boy);
+
+    System.out.println("Kilo: " + kilo + " kg");
+    System.out.println("Boy: " + boy + " m");
+    System.out.println("Vücut Kitle İndeksi (BMI): " + bmi);
+}
+
+}
