dbb61d1a50da8459579c36336286719dbd9bfda2
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-10-23 09:38:45 +0300

    Add AsciiArtIsim class for ASCII art display

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,16 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+    System.out.println("M     M  U     U  H   H  AAAAA  M     M  M     M  EEEEE  DDDD     SSSS   AAAAA  I  DDDD");
+    System.out.println("MM   MM  U     U  H   H  A   A  MM   MM  MM   MM  E      D   D   S      A   A  I  D   D");
+    System.out.println("M M M M  U     U  HHHHH  AAAAA  M M M M  M M M M  EEEE   D   D    SSS   AAAAA  I  D   D");
+    System.out.println("M  M  M  U     U  H   H  A   A  M  M  M  M  M  M  E      D   D       S  A   A  I  D   D");
+    System.out.println("M     M   UUUUU   H   H  A   A  M     M  M     M  EEEEE  DDDD    SSSS   A   A  I  DDDD");
+    System.out.println();
+    System.out.println("BBBBB   EEEEE  SSSS   TTTTT  I  L");
+    System.out.println("B    B  E     S         T    I  L");
+    System.out.println("BBBBB   EEEE   SSS      T    I  L");
+    System.out.println("B    B  E          S    T    I  L");
+    System.out.println("BBBBB   EEEEE  SSSS     T    I  LLLLL");
+}
+
+}
