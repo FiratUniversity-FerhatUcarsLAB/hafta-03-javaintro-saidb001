50fb64c429a0a9c0f60f666a4f0d6e14a6274408
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-10-23 09:32:48 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,14 @@
+public class MilKilometreTablosu {
+    public static void main(String[] args) {
+    double katsayi = 1.609;
+
+    System.out.println("Mil\t Kilometre");
+    System.out.println("------------------");
+    System.out.println("1\t " + (1 * katsayi));
+    System.out.println("5\t " + (5 * katsayi));
+    System.out.println("10\t " + (10 * katsayi));
+    System.out.println("20\t " + (20 * katsayi));
+    System.out.println("50\t " + (50 * katsayi));
+}
+
+}
